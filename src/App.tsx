0c2bfcb672cce5239dcea3f313506509d05c8c41
Romeo <kasemsitzazax@gmail.com>
2025-08-08 20:32:04 +0700

    12

--- src/App.tsx
+++ src/App.tsx
@@ -201,7 +201,27 @@ export default function ScannerPage() {
 
         <ScannerContainer>
           <Scanner
-            formats={["qr_code"]}
+            formats={[          "qr_code",
+          "micro_qr_code",
+          "rm_qr_code",
+          "maxi_code",
+          "pdf417",
+          "aztec",
+          "data_matrix",
+          "matrix_codes",
+          "dx_film_edge",
+          "databar",
+          "databar_expanded",
+          "codabar",
+          "code_39",
+          "code_93",
+          "code_128",
+          "ean_8",
+          "ean_13",
+          "itf",
+          "linear_codes",
+          "upc_a",
+          "upc_e",]}
             constraints={{ deviceId }}
             onScan={(detectedCodes) => {
               if (detectedCodes.length > 0) {
